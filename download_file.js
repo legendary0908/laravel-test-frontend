cf7c515ecd48b0acc275538e0a197de63c0a903e
maradox49 <maradox49ss@gmail.com>
2023-12-04 20:08:52 +0300

    send and download files in a safe mode

---
+++ download_file.js
@@ -0,0 +1,66 @@
+var fs = require('fs');
+
+const urlDownloadRequest = 'http://127.0.0.1:8000/downloadrequest';
+const urlDownloadFile = 'http://127.0.0.1:8000/downloadfile';
+const ONE_OFFSET = 10240;
+
+const filePath = "data.zip"
+
+const download = async (filePath) => {
+    try {
+        const requestData = {
+            filename: filePath
+        }
+        const responseRequest = await fetch(urlDownloadRequest, {
+            method: 'POST',
+            headers: {
+                'content-type': "application/json"
+            },
+            body: JSON.stringify(requestData)
+        });
+        const responseRequestData = await responseRequest.json();
+        let filesize = responseRequestData.size;
+
+        fs.open(filePath, 'w+', async (err, fd) => {
+            if (err) throw err;
+            let offset = 0;
+            while (offset < filesize) {
+                const data = {
+                    filename: filePath,
+                    offset: offset, 
+                    length: Math.min(filesize - offset, ONE_OFFSET)
+                }
+                let response ;
+                while ( true ) {
+                    response = await fetch(urlDownloadFile, {
+                        method: 'POST',
+                        headers: {
+                            'content-type': "application/json"
+                        },
+                        body: JSON.stringify(data)
+                    });
+                    if ( response.status ===200 ) break;
+                }
+
+                const responseData = await response.json();
+                const content = responseData.content;
+
+                // console.log(content);
+
+                const buffer = Buffer.from(content, 'base64');
+                // const buffer = atob(content);
+                // console.log(buffer.length);
+
+                // await fs.writeSync(fd, buffer, 0, buffer.length, offset);
+                await fs.appendFileSync(filePath, buffer)
+
+                offset += ONE_OFFSET;
+            }
+
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+download(filePath);
---
+++ send_file.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+
+const url = 'http://127.0.0.1:8000/uploadfile';
+const ONE_OFFSET = 10240;
+const filePath = "data.zip"
+
+const readBinaryData = (filePath) => {
+    fs.open(filePath, 'r', async (err, fd) => {
+        if (err) throw err;
+
+        let offset = 0;
+        let buffer = Buffer.alloc(ONE_OFFSET);
+
+        while (true) {
+            let readLength = fs.readSync(fd, buffer, 0, ONE_OFFSET, offset);
+            if (readLength === 0) break;
+            let resizedBuffer = buffer.slice(0, readLength)
+
+            const base64Data = resizedBuffer.toString('base64');
+
+            const data = {
+                filename: filePath,
+                offset: offset,
+                content: base64Data
+            };
+
+            try {
+                const response = await fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'content-type': "application/json"
+                    },
+                    body: JSON.stringify(data)
+                });
+                if ( response.status === 200)
+                    offset += ONE_OFFSET;
+            } catch (error) {
+            }
+        }
+    });
+}
+
+readBinaryData(filePath);
